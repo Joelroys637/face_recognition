9d2d46768dbff185cce198eb93326939ead1b28f
LEO JOEL ROYS <97875782+Joelroys637@users.noreply.github.com>
2025-12-07 09:14:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -187,7 +187,10 @@ def signup_page():
         if len(password) < 8:
             st.error("Password must be at least 8 characters.")
         else:
-            image_data = image_file.read() if image_file else None
+            if image_file:
+                image_data = image_file.read() if image_file else None
+            else:
+                st.error("profile picture is must upload")
             if register_user(name, username, password, email, image_data):
                 cu_mail.mail_send(email, name, username, password)
                 st.success("Signup successful! Please login.")
@@ -241,6 +244,7 @@ else:
         login_page()
     else:
         signup_page()
+
 
 
 
